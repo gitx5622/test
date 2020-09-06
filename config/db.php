d9dddfefe7e8e3fa24812d8d260e887f268cd9a5
gitx5622 <ggitau5622@gmail.com>
2020-09-06 08:01:44 +0300

    cicd

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=cicd',
     'username' => 'root',
-    'password' => 'Password123',
+    'password' => 'Password123#@!',
     'charset' => 'utf8',
     'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
     // Schema cache options (for production environment)
